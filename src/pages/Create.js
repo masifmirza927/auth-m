915e030ac8c86ff4fc7ca66dec300ebec07f050a
masifmirza927 <masifmirza927@gmail.com>
2023-06-22 10:20:45 +0500

    create form

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -2,7 +2,31 @@ import React from 'react'
 
 function Create() {
   return (
-    <>Create</>
+    <>
+      <div style={{ width: "600px" }} className='mx-auto'>
+        <div className="mb-3">
+          <label htmlFor="formFile" class="form-label">Image</label>
+          <input className="form-control" type="file" id="formFile" />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="name" className="form-label">Name</label>
+          <input type="text" className="form-control" id="name" />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">Email</label>
+          <input type="email" className="form-control" id="email" />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="address" className="form-label">Address</label>
+          <input type="text" className="form-control" id="address" />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="exampleFormControlTextarea1" className="form-label">About</label>
+          <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+        </div>
+        <button className='btn btn-primary'>Create</button>
+      </div>
+    </>
   )
 }
 
