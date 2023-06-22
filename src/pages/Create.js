11013b52820705d66bc9d8550368fa38c9b50015
masifmirza927 <masifmirza927@gmail.com>
2023-06-22 15:49:01 +0500

    file uploading

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,44 +1,85 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
+import { useFormik } from 'formik';
 
 function Create() {
-  const [name, setName] = useState("");
-  const [image, setImage] = useState("");
-  const [email, setEmail] = useState("");
-  const [address, setAddress] = useState("");
-  const [about, setAbout] = useState("");
-  const [errors, setErrors] = useState({});
-  
-  const handleSubmit = () => {
+  // const [name, setName] = useState("");
+  // const [image, setImage] = useState("");
+  // const [email, setEmail] = useState("");
+  // const [address, setAddress] = useState("");
+  // const [about, setAbout] = useState("");
+  // const eRefs = useRef({});
+  // const [errors, setErrors] = useState([]);
+  const formik = useFormik({
+    initialValues: {
+      name: "", image: "", email: "", address: "", about: ""
+    },
+    onSubmit: values => {
+      // alert(JSON.stringify(values, null, 2));
+      console.log(values)
+    },
+  });
 
-    if(name && image && email && address && about) {
-      
-      
+  // const handleSubmit = () => {
 
-    }
+  //   // if (!address)  setErrors({...errors, address: "please write your address"});
+  //   // if (!about)  setErrors({...errors, about: "write something about you"});
 
-    // if(!name) {
-    //   // const newAr = {...errors, name: "name field is required"}
-    //   setErrors(prevState => ({
-    //     ...prevState,
-    //     name: "name filed is required"
-    //   }));
-    //   console.log("inside")
-    // }
+  //   // if(name && image && email && address && about) {
 
-console.log(errors)
-  }
+
+
+  //   // }
+
+  // }
 
   return (
     <>
-      <form onSubmit={ (e) => e.preventDefault() }>
+      <form onSubmit={formik.handleSubmit}>
+      <div className="mb-3">
+        <label htmlFor="name">Student Name</label>
+        <input
+          id="name"
+          name="name"
+          type="text"
+          onChange={formik.handleChange}
+          value={formik.values.name}
+        />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="email">Email Address</label>
+          <input
+            id="email"
+            name="email"
+            type="email"
+            onChange={formik.handleChange}
+            value={formik.values.email}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="image">Image</label>
+          <input
+            id="image"
+            name="image"
+            type="file"
+            onChange={(e) => formik.setFieldValue('image', e.currentTarget.files[0])}
+          />
+        </div>
+
+
+        <button type="submit" className='btn btn-primary'>Submit</button>
+      </form>
+      {/* <form onSubmit={(e) => e.preventDefault()}>
         <div style={{ width: "600px" }} className='mx-auto'>
           <div className="mb-3">
             <label htmlFor="formFile" className="form-label">Image</label>
-            <input onChange={(e) => { setImage(e.target.files[0])} } className="form-control" type="file" id="formFile"  />
+            <input onChange={(e) => { setImage(e.target.files[0]) }} className="form-control" type="file" id="formFile" />
           </div>
           <div className="mb-3">
             <label htmlFor="name" className="form-label">Name</label>
-            <input onChange={(e) => { setName(e.target.value) }} type="text" className="form-control" id="name" />
+            <input onChange={(e) => { setName(e.target.value) }} type="text" className={`form-control ${(errors.name) ? " inputError" : ""}`} id="name" />
+            {
+              (errors.name) ? <p className='errTxt'>{errors.name}</p> : ""
+            }
           </div>
           <div className="mb-3">
             <label htmlFor="email" className="form-label">Email</label>
@@ -54,7 +95,7 @@ console.log(errors)
           </div>
           <button onClick={handleSubmit} className='btn btn-primary'>Create</button>
         </div>
-      </form>
+      </form> */}
     </>
   )
 }
